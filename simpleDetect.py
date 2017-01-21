e1d8be84580f1d7a0c34c268f8cffe3f45292df0
sven-of-the-north <zhangss@ucalgary.ca>
2017-01-21 15:38:20 -0700

    updated simpleDetect to accept command line arguments, and to enable setting modes

--- simpleDetect.py
+++ simpleDetect.py
@@ -1,9 +1,26 @@
 from SimpleCV import Image, Color
-import copy
+import os, sys, argparse
 
-img = Image("peter1.png")
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="path to image")
+ap.add_argument("-c", "--crop", default='store_false', action='store_true', help="save cropped blobs")
+ap.add_argument("-s", "--show", default='store_false', action='store_true', help="show blobs on input image")
+args = vars(ap.parse_args())
+
+img = Image(args["image"])
+crop = args["crop"]
+show = args["show"]
+
+if( crop == 'store_false' and show == 'store_false' ):
+    print ( "warning: no crop/show mode specified" )
+    sys.exit(2)
+else:
+    if not os.path.exists('croppedBlobs'):
+        os.makedirs('croppedBlobs')
 
 color_distance = img.colorDistance(Color.RED).invert() * 2.5
+# we should consider using multiple color filters; using r/g/b or custom
+# this /might/ improve accuracy
 
 binarized = color_distance.binarize()
 
@@ -13,11 +30,20 @@ blobs = binarized.findBlobs( minsize=50, maxsize=500 )
 
 overlay = color_distance.dl()
 
-print "blobs detected: ", len(blobs)
+#print "blobs detected: ", len(blobs)
+
+counter = 1
 
 for blob in blobs:
     overlay.rectangle( blob.topLeftCorner(), (blob.width(), blob.height()), Color.RED, width = 2)
+    
+    if( crop == True ):    
+        croppedBlob = img.crop( blob )
+        name = "croppedBlobs/blob" + str(counter) + ".png"
+        croppedBlob.save(name)
+        counter += 1
 
-img.addDrawingLayer(overlay)
-img.show()
+if( show == True ):
+   img.addDrawingLayer(overlay)
+   img.show()
 
